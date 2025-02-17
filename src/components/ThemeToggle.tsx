82a468b392c89fa9b5934d3388d19061fd2f4c98
Matt7500 <matttelke@gmail.com>
2025-02-17 15:48:16 -0500

    Moved theme toggle over to the left more

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,3 @@
-
 import { Moon, Sun } from "lucide-react";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
@@ -11,7 +10,7 @@ export function ThemeToggle() {
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="fixed bottom-4 right-4"
+      className="fixed bottom-4 right-8"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
