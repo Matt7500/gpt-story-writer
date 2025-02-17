1aef127734bb712d05388ac60a33e28dc3a0665e
Matt7500 <matttelke@gmail.com>
2025-02-17 15:35:11 -0500

    Updated CORS to allow delete

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -95,7 +95,7 @@ app.use(cors({
     }
     return callback(null, true);
   },
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
