347d6c57e9368a829f9b550da789f2ffdd31c25a
Matt7500 <matttelke@gmail.com>
2025-02-22 18:07:43 -0500

    Added tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	prefix: "",
 	theme: {
@@ -84,11 +83,16 @@ export default {
 					to: {
 						height: '0'
 					}
+				},
+				'slide-up': {
+					'0%': { transform: 'translateY(10px)', opacity: '0' },
+					'100%': { transform: 'translateY(0)', opacity: '1' },
 				}
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'slide-up': 'slide-up 0.2s ease-out',
 			}
 		}
 	},
@@ -96,4 +100,6 @@ export default {
 		require("tailwindcss-animate"),
 		require("tailwind-scrollbar")({ nocompatible: true }),
 	],
-} satisfies Config;
+}
+
+export default config;
